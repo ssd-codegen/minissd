230fad973322cd4cd94febde9cbd2b0dc92b09ed
hardliner66 <hardliner66@gmail.com>
2025-01-30 05:44:52 +0100

    remove windows specific stuff

--- example/minissd_test.c
+++ example/minissd_test.c
@@ -1,127 +1,115 @@
-#define ALLOC_TEST
-
-#ifdef ALLOC_TEST
-#define _CRTDBG_MAP_ALLOC
-#endif
+#include "./minissd.h"
 
 #include <stdlib.h>
-#ifdef ALLOC_TEST
-#include <crtdbg.h>
-#endif
-
-#include "./minissd.h"
 
 #include <stdio.h>
 #include <assert.h>
 
-void print_attributes(Attribute* attr)
+void print_attributes(Attribute *attr)
 {
-	while (attr)
-	{
-		printf("  Attribute: %s\n", minissd_get_attribute_name(attr));
+    while (attr)
+    {
+        printf("  Attribute: %s\n", minissd_get_attribute_name(attr));
 
-		for (Argument* arg = minissd_get_attribute_arguments(attr); arg; arg = minissd_get_next_argument(arg))
-		{
-			printf("    Argument: %s", arg->key);
-			if (arg->value)
-			{
-				printf(" = %s", arg->value);
-			}
-			printf("\n");
-		}
+        for (Argument *arg = minissd_get_attribute_arguments(attr); arg; arg = minissd_get_next_argument(arg))
+        {
+            printf("    Argument: %s", arg->key);
+            if (arg->value)
+            {
+                printf(" = %s", arg->value);
+            }
+            printf("\n");
+        }
 
-		attr = minissd_get_next_attribute(attr);
-	}
+        attr = minissd_get_next_attribute(attr);
+    }
 }
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-#ifdef ALLOC_TEST
-	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-#endif
-	if (argc != 2)
-	{
-		printf("Usage: %s <input_file>\n", argv[0]);
-		return 1;
-	}
-	FILE* f = fopen(argv[1], "rb");
-	if (!f)
-	{
-		printf("Failed to open file: %s\n", argv[1]);
-		return 2;
-	}
-	fseek(f, 0, SEEK_END);
-	long fsize = ftell(f);
-	fseek(f, 0, SEEK_SET); /* same as rewind(f); */
+    if (argc != 2)
+    {
+        printf("Usage: %s <input_file>\n", argv[0]);
+        return 1;
+    }
+    FILE *f = fopen(argv[1], "rb");
+    if (!f)
+    {
+        printf("Failed to open file: %s\n", argv[1]);
+        return 2;
+    }
+    fseek(f, 0, SEEK_END);
+    long fsize = ftell(f);
+    fseek(f, 0, SEEK_SET); /* same as rewind(f); */
 
-	char* source = malloc(fsize + 1);
-	assert(source != NULL);
+    char *source = malloc(fsize + 1);
+    assert(source != NULL);
 
-	fread(source, fsize, 1, f);
-	fclose(f);
+    fread(source, fsize, 1, f);
+    fclose(f);
 
-	source[fsize] = 0;
+    source[fsize] = 0;
 
-	Parser* parser = minissd_create_parser(source);
-	AstNode* ast = minissd_parse(parser);
+    Parser *parser = minissd_create_parser(source);
+    AstNode *ast = minissd_parse(parser);
 
-	if (!ast)
-	{
-		printf("Parsing failed: %s\n", parser->error);
-		minissd_free_parser(parser);
-		return 1;
-	}
+    if (!ast)
+    {
+        printf("Parsing failed: %s\n", parser->error);
+        minissd_free_parser(parser);
+        return 1;
+    }
 
-	for (AstNode* node = ast; node; node = minissd_get_next_node(node))
-	{
-		printf("Node Type: ");
-		switch (minissd_get_node_type(node))
-		{
-		case NODE_IMPORT:
-			printf("Import\n");
-			printf("  Path: %s\n", minissd_get_import_path(node));
-			print_attributes(minissd_get_attributes(node));
-			break;
+    for (AstNode *node = ast; node; node = minissd_get_next_node(node))
+    {
+        printf("Node Type: ");
+        switch (minissd_get_node_type(node))
+        {
+        case NODE_IMPORT:
+            printf("Import\n");
+            printf("  Path: %s\n", minissd_get_import_path(node));
+            print_attributes(minissd_get_attributes(node));
+            break;
 
-		case NODE_DATA:
-			printf("Data\n");
-			printf("  Name: %s\n", minissd_get_data_name(node));
-			print_attributes(minissd_get_attributes(node));
+        case NODE_DATA:
+            printf("Data\n");
+            printf("  Name: %s\n", minissd_get_data_name(node));
+            print_attributes(minissd_get_attributes(node));
 
-			for (Property* prop = minissd_get_properties(node); prop; prop = minissd_get_next_property(prop))
-			{
-				printf("  Property: %s : %s\n", minissd_get_property_name(prop), minissd_get_property_type(prop));
-				print_attributes(prop->attributes);
-			}
-			break;
+            for (Property *prop = minissd_get_properties(node); prop; prop = minissd_get_next_property(prop))
+            {
+                printf("  Property: %s : %s\n", minissd_get_property_name(prop), minissd_get_property_type(prop));
+                print_attributes(prop->attributes);
+            }
+            break;
 
-		case NODE_ENUM:
-			printf("Enum\n");
-			printf("  Name: %s\n", minissd_get_enum_name(node));
-			print_attributes(minissd_get_attributes(node));
+        case NODE_ENUM:
+            printf("Enum\n");
+            printf("  Name: %s\n", minissd_get_enum_name(node));
+            print_attributes(minissd_get_attributes(node));
 
-			for (EnumValue* value = minissd_get_enum_values(node); value; value = minissd_get_next_enum_value(value))
-			{
-				bool has_value;
-				int val = minissd_get_enum_value(value, &has_value);
-				printf("  Enum Value: %s", minissd_get_enum_value_name(value));
-				if (has_value)
-				{
-					printf(" = %d", val);
-				}
-				printf("\n");
-				print_attributes(value->attributes);
-			}
-			break;
+            for (EnumValue *value = minissd_get_enum_values(node); value; value = minissd_get_next_enum_value(value))
+            {
+                bool has_value;
+                int val = minissd_get_enum_value(value, &has_value);
+                printf("  Enum Value: %s", minissd_get_enum_value_name(value));
+                if (has_value)
+                {
+                    printf(" = %d", val);
+                }
+                printf("\n");
+                print_attributes(value->attributes);
+            }
+            break;
 
-		default:
-			printf("Unknown Node Type\n");
-		}
-	}
+        default:
+            printf("Unknown Node Type\n");
+        }
+    }
 
-	minissd_free_ast(ast);
-	minissd_free_parser(parser);
+    minissd_free_ast(ast);
+    minissd_free_parser(parser);
 
-	free(source);
-	return 0;
+    free(source);
+    return 0;
 }
